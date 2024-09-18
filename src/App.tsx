6d5d2ac0bf0581a0896793524f0708f584de9c13
Elkin <ersantis@uninorte.edu.co>
2024-09-18 16:10:33 -0500

    feat(UH2011): Add HomePage component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/index.css'; // Puedes usar Tailwind si decides integrarlo
+import HomePage from './pages/HomePage';
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,9 @@ const App: React.FC = () => {
 
       <main className="bg-red-200 p-6 border-2 border-gray-700">
         <p>Content (Placeholder)</p>
+        <HomePage />
       </main>
+     
 
       <footer className="bg-gray-300 p-6 border-2 border-gray-700">
         <p>Footer (Placeholder)</p>
