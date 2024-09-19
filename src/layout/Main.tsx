1a884ee792074b0eb030866d29ff1ceb76097a61
Christian Dj <christiandj456@gmail.com>
2024-09-19 16:16:18 -0500

    feat: Adjust breadcrumb styling for responsive layout and symmetric margins

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header";
 import { Subheader } from "../components/Subheader";
 import { menuItems } from "../utils/data";
+import Breadcrumb from "../components/breadcrumb";
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -8,6 +9,7 @@ export const Main = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <Subheader menuItems={menuItems} />
+        <Breadcrumb />
         <main className="flex-grow">
           {children}
         </main>
