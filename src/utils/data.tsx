26dbefe9c5bac9f6a6621bc22b08a5a6bc41fe7d
Pipe <jfbenitez@uninorte.edu.co>
2024-09-19 13:59:59 -0500

    feat(HU2007): Added url const for menu data

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -1,40 +1,40 @@
 import { MenuItemType } from "../types/MenuItem.type";
-
+const url = "/plp"
 export const menuItems: MenuItemType[] = [
   {
     category: "Tecnología",
     subcategories: [
-      { id: 1, name: "Computadores", url: "/plp" },
-      { id: 2, name: "Televisores", url: "/plp" },
-      { id: 3, name: "Audio", url: "/plp" },
-      { id: 4, name: "Video", url: "/plp" },
-      { id: 5, name: "Impresión", url: "/plp" },
-      { id: 6, name: "Cámaras", url: "/plp" },
+      { id: 1, name: "Computadores", url },
+      { id: 2, name: "Televisores", url },
+      { id: 3, name: "Audio", url },
+      { id: 4, name: "Video", url },
+      { id: 5, name: "Impresión", url },
+      { id: 6, name: "Cámaras", url },
     ],
   },
   {
     category: "Electrodomésticos",
     subcategories: [
-      { id: 7, name: "Climatización", url: "/plp" },
-      { id: 8, name: "Refrigeración", url: "/plp" },
-      { id: 9, name: "Lavadoras/Secadoras", url: "/plp" },
+      { id: 7, name: "Climatización", url },
+      { id: 8, name: "Refrigeración", url },
+      { id: 9, name: "Lavadoras/Secadoras", url },
     ],
   },
   {
     category: "Celulares",
     subcategories: [
-      { id: 10, name: "Celulares", url: "/plp" },
-      { id: 11, name: "Tabletas", url: "/plp" },
-      { id: 12, name: "Smartwatch", url: "/plp" },
+      { id: 10, name: "Celulares", url },
+      { id: 11, name: "Tabletas", url },
+      { id: 12, name: "Smartwatch", url },
     ],
   },
   {
     category: "Hogar",
     subcategories: [
-      { id: 13, name: "Salas", url: "/plp" },
-      { id: 14, name: "Comedor", url: "/plp" },
-      { id: 15, name: "Cocina", url: "/plp" },
-      { id: 16, name: "Baño", url: "/plp" },
+      { id: 13, name: "Salas", url },
+      { id: 14, name: "Comedor", url },
+      { id: 15, name: "Cocina", url },
+      { id: 16, name: "Baño", url },
     ],
   },
 ];
