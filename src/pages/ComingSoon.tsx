6519122c0c8c0e9343d8c0e094134f7959981a31
Elkin <ersantis@uninorte.edu.co>
2024-09-20 23:26:33 -0500

    fear(UH2010):Add ComingSoon page component

---
+++ src/pages/ComingSoon.tsx
@@ -0,0 +1,11 @@
+import ComingSoon from "../components/ComingSoon";
+
+export function App() {
+  return (
+    <div className="app font-poppins">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
