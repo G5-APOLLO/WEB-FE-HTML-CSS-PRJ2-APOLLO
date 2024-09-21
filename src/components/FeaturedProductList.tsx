7cd83865af55dec44a29fa5055bf42346f5a7ea8
Elkin <ersantis@uninorte.edu.co>
2024-09-20 23:31:34 -0500

    feat(UH2010)Refactor FeaturedProductList to include links to product detail pages

--- src/components/FeaturedProductList.tsx
+++ src/components/FeaturedProductList.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import FeaturedProduct from './FeaturedProduct';
 import { products } from '../data/FeaturedProducts.data';
+import { Link } from 'react-router-dom';
 const FeaturedProductsList: React.FC = () => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {products.slice(0, 12).map((product) => (
-        <FeaturedProduct key={product.id} {...product} />
+        <Link
+          to={`/pdp?optionId=${product.id}`}
+          className="text-[13px] sm:text-base block py-[7px] sm:py-[10px] px-[10px] sm:px-[15px] no-underline text-black hover:bg-[#fcfcfc]"
+        >
+          <FeaturedProduct key={product.id} {...product} />
+
+        </Link>
       ))}
     </div>
   );
