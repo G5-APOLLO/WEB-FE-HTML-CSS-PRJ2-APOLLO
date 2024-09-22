6018154fd38289a630300cb2f8a331544da6762f
Maria Del Toro <Mdeltoroa@uninorte.edu.co>
2024-09-22 17:57:15 -0500

    fix Banner main page and fix buttons footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ import { importImage } from '../utils/importImage';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-custom-radial-header text-white p-5">
-      <div className="footer-top flex justify-between items-center border-b border-white pb-2 mb-2">
-        <div className="footer-info">
+    <footer className="bg-custom-radial-header text-white p-6">
+      <div className="footer-top flex flex-col md:flex-row justify-between items-center border-b border-white pb-4 mb-4">
+        <div className="footer-info mb-4 md:mb-0">
           <p>© 2022 CLICKY S.A.S.</p>
           <p>NIT: 123456789-0</p>
         </div>
-        <div className="footer-social flex">
+        <div className="footer-social flex mb-4 md:mb-0">
           <a href="#" className="mx-2">
             <img src={importImage('facebook-svgrepo-com.svg')} alt="Facebook" width="40" height="40" />
           </a>
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
             <img src={importImage('instagram-svgrepo-com.svg')} alt="Instagram" width="40" height="40" />
           </a>
         </div>
-        <div className="footer-payment flex">
+        <div className="footer-payment flex mb-4 md:mb-0">
           <img src={importImage('pse.png')} alt="Pse" width="60" height="40" className="mx-2" />
           <img src={importImage('visa-svgrepo-com.svg')} alt="Visa" width="40" height="40" className="mx-2" />
           <img src={importImage('paypal-svgrepo-com.svg')} alt="PayPal" width="40" height="40" className="mx-2" />
@@ -34,16 +34,16 @@ const Footer: React.FC = () => {
             <Link to="/" className="text-white no-underline">Inicio</Link>
           </li>
           <li>
-            <Link to="/plp" className="text-gray-500 no-underline">Productos</Link>
+            <Link to="/cart" className="text-gray-500 no-underline">Productos</Link>
           </li>
           <li>
-            <Link to="/pdp" className="text-gray-500 no-underline">Servicios</Link>
+            <Link to="/cart" className="text-gray-500 no-underline">Servicios</Link>
           </li>
           <li>
             <Link to="/cart" className="text-gray-500 no-underline">Contacto</Link>
           </li>
           <li>
-            <Link to="/about" className="text-gray-500 no-underline">Sobre Nosotros</Link>
+            <Link to="/cart" className="text-gray-500 no-underline">Sobre Nosotros</Link>
           </li>
         </ul>
       </div>
