dbef3881a7ca848934b51a5bb9630ae3cac802e8
Elkin <ersantis@uninorte.edu.co>
2024-09-27 23:28:04 -0500

    feat(HU2018):Refactor FeaturedProductList component to use dynamic data from API

--- src/components/FeaturedProductList.tsx
+++ src/components/FeaturedProductList.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import FeaturedProduct from './FeaturedProduct';
-import { products } from '../data/FeaturedProducts.data';
+//import { products } from '../data/FeaturedProducts.data';
+import { useFeaturedProducts } from '../hooks/useFeaturedProducts';
 import { Link } from 'react-router-dom';
+import { ProductProps } from '../types/Product.type';
+
 
 const FeaturedProductsList: React.FC = () => {
+
+  const { data, error, isLoading } = useFeaturedProducts();
+  console.log(data);
+  if (isLoading) return (<p> Cargando </p>);
+  if (error) return (<p>Error</p>);
   return (
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {products.slice(0, 12).map((product) => (
+
+      {data.slice(0, 12).map((product: ProductProps) => (
         <Link
           to={`/pdp?optionId=${product.id}`}
           className="block"
