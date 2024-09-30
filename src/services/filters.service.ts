38e6eafbfccfa52e6cdd8d173a50d72f6306b581
ChristianDj456 <79539982+ChristianDj456@users.noreply.github.com>
2024-09-30 17:47:05 -0500

    Revert "Feature/hu2020 filters api"

--- src/services/filters.service.ts
+++
@@ -1,7 +0,0 @@
-import { fetcher } from "./api";
-import { FilterProps } from "../types/FiltersPlp.type";
-
-export const getFiltersbyCategory = async (optionIDfilter: string): Promise<FilterProps[]> => {
-    const endpoint = `/filters?optionIDfilter=${optionIDfilter}`;
-    return fetcher(endpoint);  
-}
