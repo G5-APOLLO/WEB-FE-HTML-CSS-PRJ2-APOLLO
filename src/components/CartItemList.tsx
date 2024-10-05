c1aee30820bdd22809fccf6aa037ff725720dbb0
Christian Dj <christiandj456@gmail.com>
2024-10-04 23:06:27 -0500

    Adding link to pdo

--- src/components/CartItemList.tsx
+++ src/components/CartItemList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importar Link de React Router
 import { Product } from '../types/CartProduct.type';
 import QuantitySelector from '../components/QuantitySelector';
 import { formatCurrency } from '../utils/formatCurrency'; // Asegúrate de que esté bien importada
@@ -20,9 +21,17 @@ const CartItemList: React.FC<CartItemListProps> = ({
     <section className="flex-1">
       {cartItems.map(item => (
         <div className="bg-white p-5 mb-4 border border-gray-200 rounded-md flex flex-col lg:flex-row justify-between items-start lg:items-center" key={item.id}>
-          <img src={item.image} alt={item.name} className="w-4/5 h-4/5 lg:w-24 lg:h-24 object-cover mb-4 lg:mb-0" />
+          {/* Imagen con enlace al PDP */}
+          <Link to={`/pdp?optionId=${item.id}&optionName=${item.name}`} className="w-4/5 h-4/5 lg:w-24 lg:h-24 object-cover mb-4 lg:mb-0">
+            <img src={item.image} alt={item.name} className="w-full h-full object-cover" />
+          </Link>
+
           <div className="flex-1 ml-0 lg:ml-5">
-            <p className="text-lg font-semibold">{item.name}</p>
+            {/* Nombre con enlace al PDP */}
+            <Link to={`/pdp?optionId=${item.id}&optionName=${item.name}`}>
+              <p className="text-lg font-semibold hover:underline">{item.name}</p>
+            </Link>
+
             <p>Selecciona un método de envío:</p>
             <div className="flex space-x-2">
               <label className="flex items-center">
@@ -63,9 +72,13 @@ const CartItemList: React.FC<CartItemListProps> = ({
               updateQuantity={(newQuantity) => updateQuantity(item.id, newQuantity)}
             />
           </div>
+
+          {/* Precio formateado */}
           <div className="text-lg font-bold text-gray-700 mt-4 lg:mt-0">
             {formatCurrency(item.discountedPrice || item.normalPrice)} {/* Formatea el precio */}
           </div>
+
+          {/* Botón de eliminar */}
           <div className="ml-0 lg:ml-5 mt-4 lg:mt-0">
             <button
               onClick={() => removeItem(item.id)}
